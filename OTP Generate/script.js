ee4bb92e17034054ef49951463f0199b1d2b51b8
MD SAJID <sheikhsajid1602@gmail.com>
2025-03-01 14:58:20 +0530

    Add files via upload

---
+++ OTP Generate/script.js
@@ -0,0 +1,78 @@
+const inp = document.getElementById("range");
+const btn = document.getElementById("btn");
+const h3 = document.getElementById("h3");
+
+const small = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+];
+btn.addEventListener("click", () => {
+  let str = "";
+  let n = small.length;
+  let val=inp.value;
+  for (let i = 0; i < val; i++) {
+    const num = Math.floor(Math.random() * n);
+    str = str + small[num];
+  }
+  h3.innerHTML=str;
+});
